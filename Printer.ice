e144f1e2c2cdaf49b799d78cd0fc8d2105a6ea43
EC2 Default User <ec2-user@ip-172-31-8-49.ec2.internal>
2026-04-13 18:42:07 +0000

    Add toLower and toHigher funcs

--- Printer.ice
+++ Printer.ice
@@ -3,5 +3,7 @@ module Demo
     interface Printer
     {
         string printString(string s);
+	string stringToLower(string s);
+        string stringToUpper(string s);
     }
 }
--- client2.py
+++ client2.py
@@ -3,8 +3,8 @@ import Demo
  
 communicator = Ice.initialize(sys.argv)
 
-base1 = communicator.stringToProxy("SimplePrinter1:tcp -h 98.90.53.6 -p 11000")
-base2 = communicator.stringToProxy("SimplePrinter2:tcp -h 98.90.53.6 -p 11000")
+base1 = communicator.stringToProxy("SimplePrinter1:tcp -h 34.201.23.181 -p 5678")
+base2 = communicator.stringToProxy("SimplePrinter2:tcp -h 34.201.23.181 -p 5678")
 printer1 = Demo.PrinterPrx.checkedCast(base1)
 printer2 = Demo.PrinterPrx.checkedCast(base2)
 if (not printer1) or (not printer2):
@@ -13,6 +13,10 @@ if (not printer1) or (not printer2):
 rep = printer1.printString("Hello World from printer1!")
 print(rep)
 rep = printer2.printString("Hello World from printer2!")
+print(rep)
+rep = printer1.stringToLower("HELLO WORLD")
+print(rep)
+rep = printer2.stringToUpper("hello world")
 print(rep)
 
 communicator.waitForShutdown()
